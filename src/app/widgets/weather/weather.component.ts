4fc4392ecefc5aa3a2926faf539a8bd7d7f81ded
zubairov1993 <58912688+zubairov1993@users.noreply.github.com>
2022-02-06 10:49:44 +0500

    'mod'

--- src/app/widgets/weather/weather.component.ts
+++ src/app/widgets/weather/weather.component.ts
@@ -14,20 +14,24 @@ export class WeatherComponent implements OnInit, OnDestroy {
   forecastResult: any = null
   allSubsscription: Subscription[] = []
   days = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'];
+  mainHour: number = 0
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
-    if(localStorage.getItem('default')) {
-      this.forecastResult = JSON.parse(localStorage.getItem('default') as string)
-    } else {
-      const getForecast = this.weatherService.getForecast('kiev').subscribe((response: any) => {
-        localStorage.setItem('default', JSON.stringify(response))
-        this.forecastResult = response
-      })
+    setTimeout(() => {
+      const storage = JSON.parse(localStorage.getItem('default') as string)
+      const currentDate = new Date(new Date().setHours(0,0,0,0))
+      const storageDate = new Date(new Date(storage?.location?.localtime).setHours(0,0,0,0))
 
-      this.allSubsscription.push(getForecast)
-    }
+      if(storage && storageDate >= currentDate) {
+        this.forecastResult = storage
+        console.log('this.forecastResult: ', this.forecastResult);
+        if(this.forecastResult) this.mainHour = new Date(this.forecastResult?.location?.localtime).getHours()
+      } else {
+        this.selectOption('kiev', 'onInit')
+      }
+    });
   }
 
   searchFn(event: any) {
@@ -38,10 +42,12 @@ export class WeatherComponent implements OnInit, OnDestroy {
     this.allSubsscription.push(searchCity)
   }
 
-  selectOption(name: string) {
+  selectOption(name: string, place: string) {
     const getForecast = this.weatherService.getForecast(name.toLocaleLowerCase()).subscribe((response: any) => {
       if(response) this.forecastResult = response
+      if(place == 'onInit') localStorage.setItem('default', JSON.stringify(response))
       console.log('this.forecastResult: ', this.forecastResult);
+      if(this.forecastResult) this.mainHour = new Date(this.forecastResult?.location?.localtime).getHours()
     })
     this.allSubsscription.push(getForecast)
   }
